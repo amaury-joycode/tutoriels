213a928bc0c64768f14229e8db515b761ceec68b
Amaury Joycode <amaury.joycode@gmail.com>
2022-11-29 08:58:07 +0100

    correction du script d'install jupyter

--- jupyter_install.ps1
+++ jupyter_install.ps1
@@ -2,4 +2,4 @@ pip install -r jupyter-requirements.txt
 jupyter contrib nbextension install
 jupyter-nbextension install rise --py --sys-prefix
 jupyter-nbextension enable rise --py --sys-prefix
-jt -t monokai -T -N -kl
+# jt -t monokai -T -N -kl
